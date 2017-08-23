85e387a9b4cd48e0d267f83560053331bf6481fa
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-08-23 18:23:16 +0100

    Added saving/loading to oracle

--- alphai_crocubot_oracle/crocubot_train.py
+++ alphai_crocubot_oracle/crocubot_train.py
@@ -10,7 +10,7 @@ FLAGS = tf.app.flags.FLAGS
 PRINT_LOSS_INTERVAL = 20
 N_TRAIN_SAMPLES = 1000
 
-def train(topology, data_source, train_x=None, train_y=None, bin_edges=None):
+def train(topology, data_source, train_x=None, train_y=None, bin_edges=None, save_path=None):
     """ Train network on either MNIST or time series data
 
     :param Topology topology:
@@ -37,7 +37,8 @@ def train(topology, data_source, train_x=None, train_y=None, bin_edges=None):
     model_initialiser = tf.global_variables_initializer()
 
     n_batches = int(N_TRAIN_SAMPLES / FLAGS.batch_size)
-    save_file_name = io.load_file_name(data_source, topology)
+    if save_path is None:
+        save_path = io.load_file_name(data_source, topology)
     saver = tf.train.Saver()
 
     # Launch the graph
@@ -46,7 +47,7 @@ def train(topology, data_source, train_x=None, train_y=None, bin_edges=None):
 
         if FLAGS.resume_training:
             try:
-                saver.restore(sess, save_file_name)
+                saver.restore(sess, save_path)
                 print("Model restored.")
             except:
                 print("Previous save file not found. Training from scratch")
@@ -79,7 +80,7 @@ def train(topology, data_source, train_x=None, train_y=None, bin_edges=None):
             if (epoch % PRINT_LOSS_INTERVAL) == 0:
                 print('Epoch', epoch, "loss:", str.format('{0:.2e}', epoch_loss), "in", str.format('{0:.2f}', time_epoch), "seconds")
 
-        save_path = saver.save(sess, save_file_name)
+        saver.save(sess, save_path)
         print("Model saved in file:", save_path)
 
     return epoch_loss_list
