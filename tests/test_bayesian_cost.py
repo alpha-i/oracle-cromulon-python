bf58f147a9030800206b13e8edcb9232b9456b22
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-08-25 17:50:56 +0100

    tests for calculate_log_bias_prior added.

--- tests/test_bayesian_cost.py
+++ tests/test_bayesian_cost.py
@@ -152,3 +152,70 @@ class TestBayesianCost(tf.test.TestCase):
             log_prior_value_computed = bayes_cost.calculate_log_weight_prior(weights, layer)
             log_prior_value_expected = np.sum(norm.logpdf(weights, scale=alpha_value))
             self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+    def test_calculate_log_bias_prior(self):
+        # FIXME the only difference between this and the previous one is the function
+        # FIXME calculate_log_weight_prior() changes to calculate_log_weight_prior().
+        # FIXME Otherwise, they are identical.
+        with self.test_session():
+            layer = 0
+            layer_name = str(layer)
+            alpha_value = 0.2
+            log_alpha_value = np.log(alpha_value).astype(np.float32)
+            log_alpha_name = 'log_alpha'
+            init_log_alpha = tf.constant_initializer(log_alpha_value)
+            with tf.variable_scope(layer_name):
+                log_alpha = tf.get_variable('log_alpha', shape=(), initializer=init_log_alpha)
+
+            log_alpha.initializer.run()
+            self.assertEquals(log_alpha.eval(), log_alpha_value)
+            log_alpha_retrieved = get_layer_variable(0, log_alpha_name)  # TODO this a now a test get_layer_variable()
+            self.assertEquals(log_alpha_retrieved.eval(), log_alpha_value)
+
+            # case 1 slab prior
+            n_input_series = 10
+            n_features_per_series = 100
+            n_output_series = 10
+            n_classification_bins = 12
+            topology = Topology(layers=None, n_series=n_input_series, n_features_per_series=n_features_per_series,
+                                n_forecasts=n_output_series, n_classification_bins=n_classification_bins)
+            use_double_gaussian_weights_prior = True
+            slab_std_dvn = 1.
+            spike_std_dvn = 0.05
+            spike_slab_weighting = 1.
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            weights = np.random.normal(size=5)
+            weights = weights.astype(np.float32)
+            log_prior_value_computed = bayes_cost.calculate_log_bias_prior(weights, layer)
+            log_prior_value_expected = np.sum(norm.logpdf(weights))
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+            # case 2 spike prior
+            slab_std_dvn = 2.  # note that we have condition that slab_std_dvn >= spike_std_dvn
+            spike_std_dvn = 1.
+            spike_slab_weighting = 0.
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            log_prior_value_computed = bayes_cost.calculate_log_weight_prior(weights, layer)
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+            # case 3 50/50 spike/slab
+            slab_std_dvn = 2.
+            spike_std_dvn = 1.
+            spike_slab_weighting = 0.5
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            log_prior_value_computed = bayes_cost.calculate_log_bias_prior(weights, layer)
+            log_prior_value_expected = np.sum(np.log(spike_slab_weighting * norm.pdf(weights / slab_std_dvn) /
+                                                     slab_std_dvn + (1. - spike_slab_weighting) *
+                                                     norm.pdf(weights / spike_std_dvn) / spike_std_dvn))
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+            # case 4 no double Gaussian prior
+            use_double_gaussian_weights_prior = False
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            log_prior_value_computed = bayes_cost.calculate_log_bias_prior(weights, layer)
+            log_prior_value_expected = np.sum(norm.logpdf(weights, scale=alpha_value))
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
