662f393d77bf02ecfee1a22a376d2d862956c5a1
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-08-25 16:47:11 +0100

    unit tests for calculate_log_weight_prior added

--- alphai_crocubot_oracle/bayesian_cost.py
+++ alphai_crocubot_oracle/bayesian_cost.py
@@ -1,9 +1,4 @@
-# The cost is used to control the learning process
-# This module is only required by crocubot_train.py
-
-
 import tensorflow as tf
-
 import alphai_crocubot_oracle.tensormaths as tm
 import alphai_crocubot_oracle.crocubot_model as cr
 
@@ -12,12 +7,12 @@ class BayesianCost(object):
     def __init__(self, topology, use_double_gaussian_weights_prior=True, slab_std_dvn=1.2, spike_std_dvn=0.05,
                  spike_slab_weighting=0.5):
         """
-        A class for computing Bayesian cost ad described in https://arxiv.org/pdf/1505.05424.pdf .
+        A class for computing Bayesian cost as described in https://arxiv.org/pdf/1505.05424.pdf .
         :param topology: A topology object that defines the topology of the network.
         :param use_double_gaussian_weights_prior:
         :param slab_std_dvn: The standard deviation of the slab (wide) Gaussian. Default value = 1.2.
         :param spike_std_dvn: The standard deviation of the spike (narrow) Gaussian. Default value = 0.5
-        :param spike_slab_weighting: The ratio of the spike to slab standard deviations. Default value = 0.5
+        :param spike_slab_weighting: The ratio of the spike(0) to slab(1) standard deviations. Default value = 0.5.
         """
         self.topology = topology
         self._use_double_gaussian_weights_prior = use_double_gaussian_weights_prior
@@ -57,16 +52,16 @@ class BayesianCost(object):
             weights = cr.compute_weights(layer, iteration=0, do_tile_weights=False)
             biases = cr.compute_biases(layer, iteration=0)
 
-            log_pw += self.calc_log_weight_prior(weights, layer)  # not needed if we're using many passes
-            log_pw += self.calc_log_bias_prior(biases, layer)
-            log_pw += self.calc_log_hyperprior(layer)
+            log_pw += self.calculate_log_weight_prior(weights, layer)  # not needed if we're using many passes
+            log_pw += self.calculate_log_bias_prior(biases, layer)
+            log_pw += self.calculate_log_hyperprior(layer)
 
-            log_qw += self.calc_log_q_prior(weights, mu_w, rho_w)
-            log_qw += self.calc_log_q_prior(biases, mu_b, rho_b)
+            log_qw += self.calculate_log_q_prior(weights, mu_w, rho_w)
+            log_qw += self.calculate_log_q_prior(biases, mu_b, rho_b)
 
         return log_pw, log_qw
 
-    def calc_log_weight_prior(self, weights, layer):  # TODO can we make these two into a single function?
+    def calculate_log_weight_prior(self, weights, layer):  # TODO can we make these two into a single function?
         """
         See Equation 7 in https://arxiv.org/pdf/1505.05424.pdf
         :param weights:
@@ -75,17 +70,18 @@ class BayesianCost(object):
         """
         if self._use_double_gaussian_weights_prior:
 
-            pwide = tm.unit_gaussian(weights / self._slab_std_dvn) / self._slab_std_dvn
-            pnarrow = tm.unit_gaussian(weights / self._spike_std_dvn) / self._spike_std_dvn
+            p_slab = tm.unit_gaussian(weights / self._slab_std_dvn) / self._slab_std_dvn
+            p_spike = tm.unit_gaussian(weights / self._spike_std_dvn) / self._spike_std_dvn
 
-            log_pw = tf.log(self._spike_slab_weighting * pwide + (1 - self._spike_slab_weighting) * pnarrow)
+            log_pw = tf.log(self._spike_slab_weighting * p_slab + (1 - self._spike_slab_weighting) * p_spike)
         else:
+            # FIXME this may be removed in the future as the double Gaussian is a better way to do things!
             log_alpha = cr.get_layer_variable(layer, 'log_alpha')
             log_pw = tm.log_gaussian_logsigma(weights, 0., log_alpha)
 
         return tf.reduce_sum(log_pw)
 
-    def calc_log_bias_prior(self, biases, layer):  # TODO can we make these two into a single function?
+    def calculate_log_bias_prior(self, biases, layer):  # TODO can we make these two into a single function?
         """
         See Equation 7 in https://arxiv.org/pdf/1505.05424.pdf
         :param biases:
@@ -94,21 +90,22 @@ class BayesianCost(object):
         """
         if self._use_double_gaussian_weights_prior:
 
-            pwide = tf.contrib.distributions.Normal(0., 1.).prob(biases / self._slab_std_dvn) / self._slab_std_dvn
-            pnarrow = tf.contrib.distributions.Normal(0., 1.).prob(biases / self._spike_std_dvn) / self._spike_std_dvn
+            p_slab = tf.contrib.distributions.Normal(0., 1.).prob(biases / self._slab_std_dvn) / self._slab_std_dvn
+            p_spike = tf.contrib.distributions.Normal(0., 1.).prob(biases / self._spike_std_dvn) / self._spike_std_dvn
 
-            log_pw = tf.log(self._spike_slab_weighting * pwide + (1 - self._spike_slab_weighting) * pnarrow)
+            log_pw = tf.log(self._spike_slab_weighting * p_slab + (1 - self._spike_slab_weighting) * p_spike)
         else:
             log_alpha = cr.get_layer_variable(layer, 'log_alpha')
             log_pw = tm.log_gaussian_logsigma(biases, 0., log_alpha)
 
         return tf.reduce_sum(log_pw)
 
-    def calc_log_hyperprior(self, layer):
+    @staticmethod
+    def calculate_log_hyperprior(self, layer):
         return - cr.get_layer_variable(layer, 'log_alpha')  # p(alpha) = 1 / alpha so log(p(alpha)) = - log(alpha)
 
     @staticmethod
-    def calc_log_q_prior(theta, mu, rho):
+    def calculate_log_q_prior(theta, mu, rho):
 
         # sigma = tf.nn.softplus(rho) #
         sigma = tf.exp(rho)
@@ -116,6 +113,7 @@ class BayesianCost(object):
 
         return tf.reduce_sum(log_qw)
 
+    @staticmethod
     def calculate_likelihood(self, truth, forecast):
         tm.MIN_LOG_LIKELIHOOD = -10  # Avoid numerical issues
         true_indices = tf.argmax(truth, axis=2)  # Dimensions [batch_size, N_LABEL_TIMESTEPS, N_LABEL_CLASSES]
--- tests/test_bayesian_cost.py
+++ tests/test_bayesian_cost.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
-
+import numpy as np
 from alphai_crocubot_oracle.bayesian_cost import BayesianCost
 from alphai_crocubot_oracle.topology import Topology
+from alphai_crocubot_oracle.crocubot_model import get_layer_variable
+from scipy.stats import norm
 
 
 class TestBayesianCost(tf.test.TestCase):
@@ -15,7 +17,7 @@ class TestBayesianCost(tf.test.TestCase):
         with self.test_session():
             for test in parameters:
                 theta, mu, rho, expected_result = test
-                actual_result = BayesianCost.calc_log_q_prior(theta, mu, rho).eval()
+                actual_result = BayesianCost.calculate_log_q_prior(theta, mu, rho).eval()
                 self.assertAlmostEqual(
                     actual_result,
                     expected_result,
@@ -86,3 +88,58 @@ class TestBayesianCost(tf.test.TestCase):
                           use_double_gaussian_weights_prior=use_double_gaussian_weights_prior,
                           slab_std_dvn=slab_std_dvn, spike_std_dvn=spike_std_dvn,
                           spike_slab_weighting=spike_slab_weighting)
+
+    def test_calculate_log_weight_prior(self):
+        with self.test_session():
+            layer = 0
+            layer_name = str(layer)
+            log_alpha_value = np.log(0.2).astype(np.float32)
+            log_alpha_name = 'log_alpha'
+            init_log_alpha = tf.constant_initializer(log_alpha_value)
+            with tf.variable_scope(layer_name):
+                log_alpha = tf.get_variable('log_alpha', shape=(), initializer=init_log_alpha)
+
+            log_alpha.initializer.run()
+            self.assertEquals(log_alpha.eval(), log_alpha_value)
+            log_alpha_retrieved = get_layer_variable(0, log_alpha_name)  # TODO this a now a test get_layer_variable()
+            self.assertEquals(log_alpha_retrieved.eval(), log_alpha_value)
+
+            # case 1 slab prior
+            n_input_series = 10
+            n_features_per_series = 100
+            n_output_series = 10
+            n_classification_bins = 12
+            topology = Topology(layers=None, n_series=n_input_series, n_features_per_series=n_features_per_series,
+                                n_forecasts=n_output_series, n_classification_bins=n_classification_bins)
+            use_double_gaussian_weights_prior = True
+            slab_std_dvn = 1.
+            spike_std_dvn = 0.05
+            spike_slab_weighting = 1.
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            weights = np.random.normal(size=5)
+            weights = weights.astype(np.float32)
+            log_prior_value_computed = bayes_cost.calculate_log_weight_prior(weights, layer)
+            log_prior_value_expected = np.sum(norm.logpdf(weights))
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+            # case 2 spike prior
+            slab_std_dvn = 2.  # note that we have condition that slab_std_dvn >= spike_std_dvn
+            spike_std_dvn = 1.
+            spike_slab_weighting = 0.
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            log_prior_value_computed = bayes_cost.calculate_log_weight_prior(weights, layer)
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
+
+            # case 3 50/50 spike/slab
+            slab_std_dvn = 2.
+            spike_std_dvn = 1.
+            spike_slab_weighting = 0.5
+            bayes_cost = BayesianCost(topology, use_double_gaussian_weights_prior, slab_std_dvn, spike_std_dvn,
+                                      spike_slab_weighting)
+            log_prior_value_computed = bayes_cost.calculate_log_weight_prior(weights, layer)
+            log_prior_value_expected = np.sum(np.log(spike_slab_weighting * norm.pdf(weights / slab_std_dvn) /
+                                                     slab_std_dvn + (1. - spike_slab_weighting) *
+                                                     norm.pdf(weights / spike_std_dvn) / spike_std_dvn))
+            self.assertAlmostEqual(log_prior_value_computed.eval(), log_prior_value_expected, places=5)
