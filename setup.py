1d0bb4823cc950675bd436bdc3dde3e91138f66f
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-08-29 16:02:53 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='alphai_oracle_crocubot',
+    version='0.0.1',
+    description='Alpha-I Crocubot',
+    author='Sreekumar Thaithara Balan, Christopher Bonnett, Fergus Simpson',
+    author_email='sreekumar.balan@alpha-i.co, christopher.bonnett@alpha-i.co, fergus.simpson@alpha-i.co',
+    packages=find_packages(exclude=['doc', 'tests*']),
+    install_requires=[
+        'tensorflow==1.3.0',
+        'numpy',
+        'pandas>=0.18.1,<=0.19.2',
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com/alpha-i/library-alphai-covariance.git@5a23a4496a58fb20dfb1781fa7bcd8951cc76786#egg=alphai_covariance-0.1.1',
+        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_finance/'
+    ]
+)
