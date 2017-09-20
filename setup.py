c26c91803cfe79c309e7022c8fca87cfebdffe75
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-09-20 12:02:57 +0100

    data sources now insided setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
         'alphai-time-series>=0.0.3',
         'alphai-finance>=1.2.0',
         'alphai_covariance>=0.1.2',
-        'tensorflow==1.3.0',
+        'alphai-data-sources>=1.0.1',
+        'tensorflow>=1.3.0',
         'numpy==1.13.1',
         'pandas>=0.18.1',
 
@@ -21,6 +22,7 @@ setup(
     dependency_links=[
         'git+ssh://git@github.com/alpha-i/library-alphai-covariance.git@5a23a4496a58fb20dfb1781fa7bcd8951cc76786#egg=alphai_covariance-0.1.1',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_finance/',
-        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_time_series/'
+        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_time_series/',
+        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai-data-sources/'
     ]
 )
