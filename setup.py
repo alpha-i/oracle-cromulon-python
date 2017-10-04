725ed36f6ce6a1188b8195f94658336bdcabcedf
Gabriele Alese <gabriele.alese@alpha-i.co>
2017-10-04 09:13:37 +0100

    Add alphai-covariance version 0.1.3 to the requirements.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'alphai-time-series>=0.0.3',
         'alphai-finance<=1.2.1',
         'pandas-market-calendars>=0.6',
-        'alphai_covariance>=0.1.2',
+        'alphai_covariance>=0.1.3',
         'alphai-data-sources>=1.0.1',
         'tensorflow>=1.3.0',
         'numpy>=1.11.3',
@@ -21,7 +21,7 @@ setup(
 
     ],
     dependency_links=[
-        'git+ssh://git@github.com/alpha-i/library-alphai-covariance.git@5a23a4496a58fb20dfb1781fa7bcd8951cc76786#egg=alphai_covariance-0.1.1',
+        'git+ssh://git@github.com/alpha-i/library-alphai-covariance.git@0.1.3#egg=alphai_covariance-0.1.3',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_finance/',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_time_series/',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai-data-sources/'
