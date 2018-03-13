2fb67b755c3d0722e09a6ecb82754e85156bea4a
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-13 12:20:22 +0000

    fix requirements

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ setup(
     author_email='sreekumar.balan@alpha-i.co, christopher.bonnett@alpha-i.co, fergus.simpson@alpha-i.co',
     packages=find_packages(exclude=['doc', 'tests*']),
     install_requires=[
-        'alphai-time-series>=0.0.3',
-        'pandas-market-calendars>=0.6',
+        'alphai_calendars',
+        'alphai_feature_generation>=2.0.0,<3.0.0',
         'alphai_covariance==0.1.4',
+        'alphai_delphi>=2.1.0,<3.0.0',
         'alphai-data-sources>=1.0.1',
-        'tensorflow>=1.4.0',
+       ' tensorflow==1.4.0',
         'numpy>=1.12.0',
         'pandas==0.18.1',
         'scikit-learn',
-        'alphai_feature_generation==1.4.0-dev-gym'
     ],
     dependency_links=[
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_covariance/',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_finance/',
-        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_time_series/',
         'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai-data-sources/',
-        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_feature_generation/'
+        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_feature_generation/',
+        'https://pypi.fury.io/zNzsk7gQsYY335HLzW9x/alpha-i/alphai_delphi/'
     ]
 )
